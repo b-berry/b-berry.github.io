39afdda633f8d07f735f692c7a105bad2864f74a
Bryan Berry <bryan@endpoint.com>
2016-12-04 14:16:48 -0600

    maps: Added locations && codeAddress() geocoder
    * Note: NOT FUNCTIONAL as of yet

--- js/grayscale.js
+++ js/grayscale.js
@@ -34,21 +34,48 @@ $('.navbar-collapse ul li a').click(function() {
 
 // Google Maps Scripts
 var map = null;
+var lat_init = 0;
+var lng_init = 0;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(lat_init,lng_init));
 });
+
+// Google Maps Geocode
+function codeAddress(loc) 
+{
+  geocoder.geocode( {address:loc.address}, function(results, status) 
+  {
+    if (status == google.maps.GeocoderStatus.OK) 
+    {
+      //map.setCenter(results[0].geometry.location);//center the map over the result
+      ////place a marker at the location
+      //var marker = new google.maps.Marker(
+      //{
+      //    map: map,
+      //    position: results[0].geometry.location
+      //});
+      //append results
+      loc.location = {  'lat': results[0].geometry.location.lat, 
+                        'lng' : results[0].geometry.location.lng  }
+      return loc
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+   }
+  });
+}
 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 1,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        //center: new google.maps.LatLng(40.6700, -73.9400),    // New York
+        center: new google.maps.LatLng(lat_init,lng_init),      // Globe
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -175,11 +202,87 @@ function init() {
     map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
+    //var image = 'img/map-marker.png';
+    //var kyLatLn  = new google.maps.LatLng(40.6700, -73.9400);
+    //var beachMarker = new google.maps.Marker({
+    //    position: myLatLng,
+    //    map: map,
+    //    icon: image
+    //});
+
+    // Create my Google Map
+    var locations = [
+                        {   'address': 'New York, NY',  
+                            'styleUrl': 'img/marker-office.png' },
+                        {   'address': 'Brooklyn, NY',
+                            'styleUrl': 'img/marker-home.png' },
+                        {   'address': 'San Diego, CA',
+                            'styleUrl': 'img/marker-home.png' },
+                        {   'address': 'Seattle, WA',
+                            'styleUrl': 'img/marker-star.png' },
+                            // Sail
+                        {   'address': 'Jersey City, NJ',
+                            'styleUrl': 'img/marker-boat1.png' },
+                        {   'address': 'Port Washington, NY',
+                            'styleUrl': 'img/marker-boat1.png' },
+                            // MotoTrip2016-NXNE
+                        {   'address': 'Minneapolis, MN',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Hayward, WI',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Grand Marais, MI',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'South Baymouth, ON',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Elora, ON',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Batavia, NY',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Bainbridge, NY',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Hartford, CT',
+                            'styleUrl': 'img/marker-moto1.png' },
+                        {   'address': 'Port Washington, NY',  
+                            'styleUrl': 'img/marker-moto1.png' },
+                            // MotoTrip2016-SXSW
+                        {   'address': 'Cape May, NJ',
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'Elizabeth City, NC',  
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'Jacksonville, NC',
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'Aiken, GA',
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'Montgomery, AL',
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'New Orleans, LA',
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'Beaumont, TX',
+                            'styleUrl': 'img/marker-moto2.png' },
+                        {   'address': 'Austin, TX',
+                            'styleUrl': 'img/marker-moto2.png' }
+                    ];
+
+
+    //declare marker call it 'i'
+    var poi, i;
+
+    //add marker to each location poi
+    for (i = 0; i < locations.length; i++) {
+
+        //testing
+        //console.log(locations[i]['address']);
+
+        //geocode location i w/ timeout .5s (for API key restriction)
+        result = setTimeout(codeAddress(locations[i]),500};        
+        //append results
+        locations[i].lat = result.loc['lat']
+        locations[i].lng = result.lng['lng']
+ 
+        poi = new google.maps.Marker({
+                    position: new google.maps.LatLng(locations[i]['lat'], locations[i]['lng']),
+                    map: map,
+                    icon: locations[i]['styleUrl']
+                });
+    }
 }
